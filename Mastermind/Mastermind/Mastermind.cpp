5387bb0875513f66d08365dfb9afe7c2e82e9c43
jedineeson <1810027@bart.ca>
2018-05-03 18:55:38 -0400

    showgrid()

--- Mastermind/Mastermind/Mastermind.cpp
+++ Mastermind/Mastermind/Mastermind.cpp
@@ -11,8 +11,9 @@ std::random_device depart;
 std::mt19937 hasard(depart());
 std::uniform_int_distribution<> choix(0, 5);
 
-void play(std::vector<std::vector<char>>& grid, std::vector<char>& master);
+int play(std::vector<std::vector<char>>& grid, std::vector<char>& master);
 void rules();
+void showgrid(std::vector<std::vector<char>>& grid);
 
 int main()
 {
@@ -37,7 +38,7 @@ int main()
 
 }
 
-void play(std::vector<std::vector<char>>& grid, std::vector<char>& master)
+int play(std::vector<std::vector<char>>& grid, std::vector<char>& master)
 {
 	int round;
 	round = 0;
@@ -64,6 +65,8 @@ void play(std::vector<std::vector<char>>& grid, std::vector<char>& master)
 
 		round += 1;
 	} while (round == 10 || grid[round][0] == master[0] && grid[round][1] == master[1] && grid[round][2] == master[2] && grid[round][3] == master[3]);
+
+	return round;
 }
 	
 void rules()
@@ -91,5 +94,20 @@ void rules()
 	std::cout << "À chaque tentative le coffre affiche combien de lettres sont exactes et bien placées(BP) et combien de lettres sont exactes et mal placées." << std::endl;
 	std::cout << "Bonne chance étranger!" << std::endl;
 	std::cout << "-----------------------------------" << std::endl;
+}
+
+void showgrid(std::vector<std::vector<char>>& grid)
+{
+	std::cout << "     " << "     " << "     " << "    " << "[ BP  MP ]" << std::endl;
+	std::cout << "[" << grid[1][1] << "][" << grid[1][2] << "][" << grid[1][3] << "][" << grid[1][4] << "] - [" << grid[1][5] << "][" << grid[1][6] << "]" << std::endl;
+	std::cout << "[" << grid[2][1] << "][" << grid[2][2] << "][" << grid[2][3] << "][" << grid[2][4] << "] - [" << grid[2][5] << "][" << grid[2][6] << "]" << std::endl;
+	std::cout << "[" << grid[3][1] << "][" << grid[3][2] << "][" << grid[3][3] << "][" << grid[3][4] << "] - [" << grid[3][5] << "][" << grid[3][6] << "]" << std::endl;
+	std::cout << "[" << grid[4][1] << "][" << grid[4][2] << "][" << grid[4][3] << "][" << grid[4][4] << "] - [" << grid[4][5] << "][" << grid[4][6] << "]" << std::endl;
+	std::cout << "[" << grid[5][1] << "][" << grid[5][2] << "][" << grid[5][3] << "][" << grid[5][4] << "] - [" << grid[5][5] << "][" << grid[5][6] << "]" << std::endl;
+	std::cout << "[" << grid[6][1] << "][" << grid[6][2] << "][" << grid[6][3] << "][" << grid[6][4] << "] - [" << grid[6][5] << "][" << grid[6][6] << "]" << std::endl;
+	std::cout << "[" << grid[7][1] << "][" << grid[7][2] << "][" << grid[7][3] << "][" << grid[7][4] << "] - [" << grid[7][5] << "][" << grid[7][6] << "]" << std::endl;
+	std::cout << "[" << grid[8][1] << "][" << grid[8][2] << "][" << grid[8][3] << "][" << grid[8][4] << "] - [" << grid[8][5] << "][" << grid[8][6] << "]" << std::endl;
+	std::cout << "[" << grid[9][1] << "][" << grid[9][2] << "][" << grid[9][3] << "][" << grid[9][4] << "] - [" << grid[9][5] << "][" << grid[9][6] << "]" << std::endl;
+	std::cout << "[" << grid[10][1] << "][" << grid[10][2] << "][" << grid[10][3] << "][" << grid[10][4] << "] - [" << grid[10][5] << "][" << grid[10][6] << "]" << std::endl;
 }
 
